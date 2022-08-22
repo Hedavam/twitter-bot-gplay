53c19de95da564e7d2bbc126c7fe1ed9b6e3fd10
Hedavam <48224757+Hedavam@users.noreply.github.com>
2022-08-22 12:08:09 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'pqyHYrQ0gaViGLd7diCMSHV85',
+    consumer_secret: 'c3vssddBDdIrGOMKV9B673tefy8Acb1oKAOc0QMDqczvLGVMEK',
+    access_token_key: '1516310318208724996-k5yXwY0gx65eoqEephDYS9VIK6eNx0',
+    access_token_secret: 'NdcEJZ7ZC1tvwimve1dUWLWGyDAXFo7aNEsNIKLQuxAXp'
+  }
